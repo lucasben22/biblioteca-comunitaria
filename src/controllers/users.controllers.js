0b8883fb517224c337208a383dc7aa0362ecf600
panzazeta <76927307+panzazeta@users.noreply.github.com>
2025-04-17 14:42:53 -0300

    add code ok

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -9,3 +9,18 @@ export const getUsers = async (req, res) => {
             res.status(400).send({answer: "Error", message: error})
     }
 }
+
+export const getUserById = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id);
+
+        if (user) {
+            res.status(200).send({ answer: "Ok", message: user });
+        } else {
+            res.status(404).send({ answer: "Error", message: "User not found" });
+        }
+    } catch (error) {
+        res.status(400).send({ answer: "Error", message: error.message });
+    }
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
-import { getUsers } from "../controllers/users.controllers.js";
+import { getUsers, getUserById } from "../controllers/users.controllers.js";
 import { Router } from "express"
 
 const userRouter = Router();
 
 userRouter.get ("/", getUsers);
+userRouter.get ("/:id", getUserById);
 
 export default userRouter
